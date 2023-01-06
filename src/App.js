c82b7659dec204a538943b213a7bd59b82a5823d
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-06 12:48:32 +0000

    fetch users

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from "react"
+import "./index.scss"
+import { Success } from "./components/Success"
+import { Users } from "./components/Users"
+import { useEffect } from "react"
+import { useState } from "react"
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        setIsLoading(true)
+        const getUsers = async () => {
+            const res = await fetch("https://reqres.in/api/users")
+            const { data } = await res.json()
+            setData(data)
+        }
+        getUsers()
+        setIsLoading(false)
+    }, [])
+    console.log(data)
+
+    return (
+        <div className="App">
+            <Users isLoading={isLoading} items={data} />
+            {/* <Success /> */}
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
 export const Users = ({ items, isLoading }) => {
+  const [text,setText]=useState('')
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Найти пользователя..." />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -19,7 +21,9 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items.filter(item=>item.first_name.toLowerCase().includes(text.toLowerCase())).map((user) => (
+            <User key={user.id} {...user}/>
+          ))}
         </ul>
       )}
       <button className="send-invite-btn">Отправить приглашение</button>
